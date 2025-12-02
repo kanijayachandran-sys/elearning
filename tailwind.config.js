f44ec0b8b4488e75af4e901a5b3fee6c15444a1b
Kani Jayachandran <kanijayachandran25@gmail.com>
2025-12-02 19:39:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Tell Tailwind to scan all React files for class names
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
